728de2af00f85dec10254e2ac6a26005ab24b36c
jonsebas15 <johan_1115@hotmail.com>
2023-07-01 00:36:40 -0500

    Creacion del token y arreglo en la parte del post datos del usuario

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,18 +1,35 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import {creaciontoken} from '../libs/jwt.js';
 
 export const register = async(req, res) => {
    const {nombres, apellidos, correo, tipo_cedula, cedula, contrasena} = req.body
     try{
+
+        const contHash = await bcrypt.hash(contrasena, 5)
+
         const newUser = new User({
             nombres,
             apellidos,
             correo,
             tipo_cedula,
             cedula,
-            contrasena
+            contrasena: contHash, //sirve para encriptar la contraseña. pero no funciona (utiliza npm i bcrypt)--- YA lo encontre, sale en la respuesta mas no en la terminal consola.
         })
-        await newUser.save(); //es asincrono por ende lleva un await y lleva un async todo lo que lo qu elo contenga en este caso la linea 3
-        res.send("Registrando");
+        const Usersaved = await newUser.save(); //es asincrono por ende lleva un await y lleva un async todo lo que lo qu elo contenga en este caso la linea 3
+
+        //creacion del token, para mostrar el token es como res.json({token})
+        const token = await creaciontoken({id:Usersaved.id})
+        //creacion de una cookie 
+        res.cookie("token", token);
+        
+        //res.json(Usersaved); para que salga todos los datos en la respuesta --- el siguiente es para que salga solo algunos
+        res.json({
+            id:Usersaved._id,
+            nombres:Usersaved.nombres,
+            apellidos:Usersaved.apellidos,
+            correo:Usersaved.correo
+        })
         console.log(req.body);
     }catch (error){
         console.log(error);
